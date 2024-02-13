35586538ad31f93304843ef3e635a81c645a7ab3
balbo97 <balbosauro1@gmail.com>
2024-02-13 17:51:41 +1000

    comics.php added

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -23,7 +23,7 @@
     {{-- MAIN  --}}
     <main>
         {{-- JUMBOTRONE --}}
-        <div class="jumbotrone"></div>
+        @include('partials.jumbotrone')
 
         {{-- COMICS  --}}
         <div class="bg-dark">
@@ -47,25 +47,7 @@
         </div>
 
         {{-- BLU BAR  --}}
-        <div class="buy_comics">
-            <div class="container-sm">
-                <div class="row">
-                    <div class="col">
-                        <div class="images d-flex justify-content-around">
-
-                            <img src="{{ Vite::asset('../resources/img/buy-comics-digital-comics.png') }}"
-                                alt="">
-                            <img src="{{ Vite::asset('../resources/img/buy-comics-merchandise.png') }}" alt="">
-                            <img src="{{ Vite::asset('../resources/img/buy-comics-shop-locator.png') }}" alt="">
-                            <img src="{{ Vite::asset('../resources/img/buy-comics-subscriptions.png') }}"alt="">
-
-                            <img src="{{ Vite::asset('../resources/img/buy-dc-power-visa.svg') }}" alt="">
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('partials.blu_bar')
     </main>
 
     {{-- FOOTER  --}}
---
+++ resources/views/partials/blu_bar.blade.php
@@ -0,0 +1,18 @@
+<div class="buy_comics">
+    <div class="container-sm">
+        <div class="row">
+            <div class="col">
+                <div class="images d-flex justify-content-around">
+
+                    <img src="{{ Vite::asset('../resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <img src="{{ Vite::asset('../resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <img src="{{ Vite::asset('../resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <img src="{{ Vite::asset('../resources/img/buy-comics-subscriptions.png') }}"alt="">
+
+                    <img src="{{ Vite::asset('../resources/img/buy-dc-power-visa.svg') }}" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
