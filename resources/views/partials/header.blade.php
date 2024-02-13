1ecc02f5330cffd82d4a951e95fdcccba7f7c8c0
balbo97 <balbosauro1@gmail.com>
2024-02-13 16:30:45 +1000

    partials.header created and included

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,48 @@
+<header>
+    <div class="container-sm">
+        <div class="row">
+            <div class="col">
+                <div class="navbar">
+
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo">
+                    </a>
+
+                    <ul class="d-flex list-unstyled">
+                        <li>
+                            <a href="">Charaters</a>
+                        </li>
+                        <li>
+                            <a href="">Comics</a>
+                        </li>
+                        <li>
+                            <a href="">Movies</a>
+                        </li>
+                        <li>
+                            <a href="">Tv</a>
+                        </li>
+                        <li>
+                            <a href="">Games</a>
+                        </li>
+                        <li>
+                            <a href="">Collectibles</a>
+                        </li>
+                        <li>
+                            <a href="">Videos</a>
+                        </li>
+                        <li>
+                            <a href="">Fans</a>
+                        </li>
+                        <li>
+                            <a href="">News</a>
+                        </li>
+                        <li>
+                            <a href="">Shop</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
